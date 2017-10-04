f1e7b2ec4378e3d7caa3d6fde134cbdf6c1d7115
David Aronson <daronson@kewforest.org>
2017-10-04 13:53:19 -0400

    done one

---
+++ one.py
@@ -0,0 +1,18 @@
+import turtle
+
+def make_squares(turt,size,num):
+    
+    for j in range(num):
+        turt.pendown()
+        for i in range(4):
+            turt.forward(size)
+            turt.left(90)
+        turt.penup()
+        turt.forward(2*size)
+
+wn = turtle.Screen()
+owen=turtle.Turtle()
+
+
+
+make_squares(owen,20,5)
