cf1a8ce317146906b7e01d2ac7dcee790e37df7f
David Aronson <daronson@kewforest.org>
2017-10-05 08:25:06 -0400

    two complete

--- two.py
+++ two.py
@@ -1,18 +1,24 @@
 import turtle
 
 def make_squares(turt,size,num):
-    
+    """
+    makes cocentric squares. size is the size of the
+    inner most and also half the distance between squares,
+    num is the number of squares
+    """
+    origSize = size
     for j in range(num):
         turt.pendown()
+        
         for i in range(4):
             turt.forward(size)
             turt.left(90)
         turt.penup()
-        turt.backward(size/2)
+        turt.backward(origSize/2)
         turt.right(90)
-        turt.forward(size/2)
+        turt.forward(origSize/2)
         turt.left(90)
-        size = size * 2
+        size = size + origSize
         
 
 wn = turtle.Screen()
@@ -21,6 +27,7 @@ wn.bgcolor("lightgreen")
 owen=turtle.Turtle()
 owen.color("hotpink")
 owen.pensize(3)
+owen.speed(10)
 
 
 make_squares(owen,20,5)
